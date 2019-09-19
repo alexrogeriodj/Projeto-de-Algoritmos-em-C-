0ea5f57617ccab13be4441830bbd809316a50cee
alexrogeriodj <alexrogeriodj@gmail.com>
2019-09-18 22:17:34 -0300

    Add files via upload

---
+++ Z-Buffer Algorithm/Coefficient_Of_Polygon.cs
@@ -0,0 +1,19 @@
+Coefficient_Of_Polygon.cs
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+ 
+namespace Graphics3Dto2D
+{
+    class Coefficient_Of_Plane
+    {
+        public double A, B, C, D;
+        public Coefficient_Of_Plane()
+        {
+        }
+            A = B = C = D = 0;
+    }
+}
